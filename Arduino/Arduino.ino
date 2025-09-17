c087951165270362909503297643c801037b5493
Matias Soria <matuleosoria221@gmail.com>
2025-09-16 21:19:35 -0300

    Agrega librerias para la implementación de pantalla lcd y vinculando el uso de teclado y pantalla en conjunto.

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,4 +1,6 @@
-#include <keypad.h>
+#include <keypad.h>//libreria para teclado matricial
+#include <Wire.h>//libreria para comunicación I2C
+#include <LiquidCrystal_I2C.h>//libreria para modulo LCD con I2C
 
 const int filas 4;
 const int columnas 4;
@@ -8,19 +10,48 @@ char teclado[filas][columnas]{
   {"7","8","9","C"}
   {"*","0","#","D"}
 };
-int filaspins [filas] {8,9,10,11};
-int columnaspins [columnas] {4,5,6,7};
+byte filaspins [filas] = {8,9,10,11};
+byte columnaspins [columnas] = {4,5,6,7};
 
 Keypad pad = Keypad(makeKeymap(teclado),filaspins,columnaspins,filas,columnas); 
-
+//Crear el objeto lcd  dirección  0x3F y 16 columnas x 2 filas
+LiquidCrystal_I2C lcd(0x3F,16,2);
 char lectura;
 
 void setup() {
-  serial.begin(9600);
-
+  Serial.begin(9600);
+  
+  // Inicializar el LCD
+  lcd.init();
+  
+  // Encender la luz de fondo
+  lcd.backlight();
+  
+  // Mostrar mensaje inicial
+  lcd.setCursor(0, 0);
+  lcd.print("Sistema Listo");
+  lcd.setCursor(0, 1);
+  lcd.print("Presiona tecla:");
 }
 
 void loop() {
- lectura = pad.getKey(); 
-
+  // Leer tecla presionada
+  lectura = pad.getKey();
+  
+  // Validar que se haya leído una tecla válida
+  if (lectura != NO_KEY) {
+    // Limpiar la línea donde se mostrará la tecla
+    lcd.setCursor(0, 1);
+    lcd.print("Tecla: ");
+    
+    // Mostrar la tecla presionada
+    lcd.print(lectura);
+    
+    // También enviar por serial para debug
+    Serial.print("Tecla presionada: ");
+    Serial.println(lectura);
+    
+    // Pequeña pausa para evitar lecturas múltiples
+    delay(200);
+  }
 }
